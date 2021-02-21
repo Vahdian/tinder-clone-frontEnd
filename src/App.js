d0d5569df0ce08d1b35d4b6529beb60c9b6a07e2
vahdian <germanpalero@gmail.com>
2021-02-21 02:08:42 +0100

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
 import { Cards } from './Cards/Cards';
-import {Header} from './Header/Header'
+import {Header} from './Header/Header';
+import {SwipeButtons} from './SwipeButtons/SwipeButtons'
 function App() {
   return (
     <div className="App">
      <Header/>
      <Cards/>
-      {/* SwipeButtons */} 
+      <SwipeButtons/>
     </div>
   );
 }
